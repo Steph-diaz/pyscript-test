0095d9c75dd26f6efd62fd59fff04b4904cf2084
stephanie <bfssi.stephanie.diaz@gmail.com>
2022-06-02 14:07:22 -0400

    added plot for loblaws

--- python/validation.py
+++ python/validation.py
@@ -62,10 +62,48 @@ pyscript.write('cal-noM', calories_nomatch)
 satfat_match = len(match_df[match_df['saturatedfat_exact_match'] == True])
 satfat_nomatch = len(match_df[match_df['saturatedfat_exact_match'] == False])
 # print('satfat', satfat_match, satfat_nomatch)
+pyscript.write('sat-match', satfat_match)
+pyscript.write('sat-noM', satfat_nomatch)
 
 sodium_match  = len(match_df[match_df['sodium_exact_match'] == True])
 sodium_nomatch = len(match_df[match_df['sodium_exact_match'] == False])
 # print('sodium', sodium_match, sodium_nomatch)
+pyscript.write('sod-match', sodium_match)
+pyscript.write('sod-noM', sodium_nomatch)
 
 plot_data = {'match': [satfat_match, calories_match, sodium_match], 'no match': [satfat_nomatch, calories_nomatch, sodium_nomatch]}
 satfat = pd.DataFrame(plot_data, index = ['sat_fat', 'calories', 'sodium'])
+
+# pyscript.write('plot', satfat.plot.bar(rot=0))
+# import matplotlib.pyplot as plt
+import numpy as np
+
+labels = ['sat_fat', 'calories', 'sodium']
+match = [satfat_match, calories_match, sodium_match]
+no_match = [satfat_nomatch, calories_nomatch, sodium_nomatch]
+
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, match, width, label='Match', color= 'forestgreen')
+rects2 = ax.bar(x + width/2, no_match, width, label='No Match', color='darkred')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('# of Products')
+ax.set_title('Loblaws vs Mintel', pad=20)
+ax.set_xticks(x, labels)
+ax.legend()
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+# ax.spines['bottom'].set_visible(False)
+# ax.spines['left'].set_visible(False)
+
+ax.bar_label(rects1, padding=3)
+ax.bar_label(rects2, padding=3)
+
+fig.tight_layout()
+
+# plt.show()
+pyscript.write('plot', fig)
